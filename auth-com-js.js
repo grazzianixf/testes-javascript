eaa1f2d987c01d984d0bc3d525c4877d4647a08c
Grazziani Xavier <grazzianixf@gmail.com>
2021-08-03 16:57:08 -0300

    novas alterações para verificar funcionamento da estrutura simulando se esta ou nao autenticado

--- auth-com-js.js
+++ auth-com-js.js
@@ -1,49 +1,70 @@
+let htmlMain = `
+<table width="100%" border="1">
+    <tr>
+        <td colspan="2" id="headerContainer" align="right">HEADER</td>
+    </tr>
+    <tr>
+        <td valign="top" align="center" id="menuContainer"></td>
+        <td id="mainContainer"></td>
+    </tr>
+</table>
+`
+//<script src="auth-com-js.js"><\/script>
 
+let htmlHeaderLogout = `
+<div>
+    User: nome <button onclick="logout()">Logout</button>
+</div>
+`
+
+let htmlHeaderLogin = `
+<div>
+    User: <input type="text" name="user" id="inputUser"> 
+    Password : <input type="text" name="password" id="inputPassword"> 
+    <button onclick="login()">Login</button>
+</div>
+`
+
+let htmlNotAllowed = `
+<div align="center">
+    <p>Not Allowed. Please, proceed to Login!!!</p>
+</div>
+`
 
 loadMain = () => {
-    let html = `
-    <table width="100%" border="1">
-        <tr>
-            <td colspan="2" id="headerContainer" align="right">HEADER</td>
-        </tr>
-        <tr>
-            <td id="menuContainer">MENU</td>
-            <td id="mainContainer"></td>
-        </tr>
-    </table>
-    `
-    //<script src="auth-com-js.js"><\/script>
 
     let container = document.getElementById("container");
 
     let body = document.getElementsByTagName("body")[0];
-    body.innerHTML = html;
+    body.innerHTML = htmlMain;
 
     let mainContainer = document.getElementById("mainContainer")
-    mainContainer.appendChild(container)
+
+    if (isAuthenticated()) {
+        loadMenu()
+        mainContainer.appendChild(container)
+    } else {
+        mainContainer.innerHTML = htmlNotAllowed
+    }
 }
 
 loadHeader = () => {
     let html = ''
 
-    if (localStorage.getItem("token")) {
-        html = `
-            <div>
-                User: nome <button onclick="logout()">Logout</button>
-            </div>
-        `
+    if (isAuthenticated()) {
+        html = htmlHeaderLogout
     } else {
-        html = `
-            <div>
-                User: <input type="text" name="user" id="inputUser"> 
-                Password : <input type="text" name="password" id="inputPassword"> 
-                <button onclick="login()">Login</button>
-            </div>
-        `
+        html = htmlHeaderLogin
     }
 
     let headerContainer = document.getElementById("headerContainer")
     headerContainer.innerHTML = html
+}
+
+loadMenu = () => {
+    let menuContainer = document.getElementById("menuContainer")
+    menuContainer.setAttribute("width", "20%")
+    menuContainer.innerHTML = "MENU"
 }
 
 login = () => {
@@ -52,13 +73,17 @@ login = () => {
 
     localStorage.setItem("token", inputUser.value + "." + inputPassword.value)
 
-    loadHeader()
+    document.location.reload();
 }
 
 logout = () => {
     localStorage.removeItem("token")
 
-    loadHeader()
+    document.location.reload();
+}
+
+isAuthenticated = () => {
+    return (localStorage.getItem("token") && localStorage.getItem("token") !== 'undefined')
 }
 
 loadMain()
